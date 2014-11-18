a703355919a4da460a0b4941c5885057df1e452c
Carlos Martín Nieto <cmn@dwim.me>
2014-11-18 16:39:44 +0100

    Move project to maven, add fake test

---
+++ src/test/java/OffloadTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import org.junit.Assert;
+/**
+ *
+ */
+public class OffloadTest {
+    @Test
+    public void notReallyATest() {
+        Assert.assertEquals("foo", "bar");
+    }
+}
