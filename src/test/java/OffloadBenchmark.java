aa27a43e115bb02c5d037d40d8252b91ecfa78ef
Carlos Martín Nieto <cmn@dwim.me>
2015-01-13 12:40:14 +0100

    Add benchmark for 13 nodes and 19 edges

--- src/test/java/OffloadBenchmark.java
+++ src/test/java/OffloadBenchmark.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 
 public class OffloadBenchmark extends AbstractBenchmark {
-	Offload.Node a, b, c, d, e, f;
+	Offload.Node a, b, c, d, e, f, g, h, i, j, k, l, m, n, o;
 
 	@Test
 	@BenchmarkOptions(benchmarkRounds = 5, warmupRounds = 1)
@@ -33,5 +33,49 @@ public class OffloadBenchmark extends AbstractBenchmark {
 		Offload.Result result = offload.optimize();
 		Assert.assertEquals(new HashSet(Arrays.asList(a, b)), result.local);
 		Assert.assertEquals(new HashSet(Arrays.asList(c, d, e, f)), result.remote);
+	}
+
+	@Test
+	@BenchmarkOptions(benchmarkRounds = 5, warmupRounds = 1)
+	public void testLargishGraph()
+		throws Exception {
+		a = new Offload.Node(5, 5, false);
+		b = new Offload.Node(5, 5, false);
+		c = new Offload.Node(5, 5);
+		d = new Offload.Node(5, 5);
+		e = new Offload.Node(5, 5);
+		f = new Offload.Node(5, 5, false);
+		g = new Offload.Node(5, 5);
+		h = new Offload.Node(5, 5);
+		i = new Offload.Node(5, 5);
+		j = new Offload.Node(5, 5);
+		k = new Offload.Node(5, 5);
+		l = new Offload.Node(5, 5);
+		m = new Offload.Node(5, 5);
+		n = new Offload.Node(5, 5);
+		o = new Offload.Node(5, 5);
+
+		a.addEdge(b, 2);
+		a.addEdge(e, 2);
+		a.addEdge(f, 2);
+		b.addEdge(c, 2);
+		c.addEdge(d, 2);
+		d.addEdge(j, 2);
+		e.addEdge(g, 2);
+		f.addEdge(g, 2);
+		f.addEdge(h, 2);
+		g.addEdge(i, 2);
+		h.addEdge(i, 2);
+		h.addEdge(j, 2);
+		i.addEdge(l, 2);
+		j.addEdge(k, 2);
+		k.addEdge(m, 2);
+		k.addEdge(n, 2);
+		l.addEdge(m, 2);
+		m.addEdge(o, 2);
+		n.addEdge(o, 2);
+
+		Offload offload = new Offload(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o);
+		Offload.Result result = offload.optimize();
 	}
 }
